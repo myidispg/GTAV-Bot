6d451f321f0ef5adf841e9fb7bede436865a0d3c
myidispg <myidispg@gmail.com>
2018-11-15 18:56:55 +0530

    Working on screen grab

--- image-grab.py
+++ image-grab.py
@@ -12,16 +12,24 @@ import cv2
 import time
 import matplotlib.pyplot as plt
 
-def screen_record(): 
+def process_img(image):
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    return processed_img
+
+def main():
     last_time = time.time()
-    while(True):
-        # 800x600 windowed mode
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-        print('loop took {} seconds'.format(time.time()-last_time))
+    while True:
+        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        #print('Frame took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        new_screen = process_img(screen)
+        cv2.imshow('window', new_screen)
+        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
         
-screen_record()
+main()
