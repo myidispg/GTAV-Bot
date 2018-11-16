83a23917e4d944d89ee26e6ddd3a88f36ed595e4
Prashnt Goyal <myidispg@gmail.com>
2018-11-16 10:02:21 +0530

    Got the screenrecording working

--- image-grab.py
+++ image-grab.py
@@ -7,7 +7,7 @@ Created on Thu Nov 15 17:59:48 2018
 """
 
 import numpy as np
-import pyscreenshot as ImageGrab
+from PIL import ImageGrab
 import cv2
 import time
 import matplotlib.pyplot as plt
@@ -20,15 +20,16 @@ def process_img(image):
     return processed_img
 
 def main():
-    last_time = time.time()
+#    last_time = time.time()
     while True:
+        # Get the top left 800x640 screen section
         screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-        #print('Frame took {} seconds'.format(time.time()-last_time))
-        last_time = time.time()
+#        print('Frame took {} seconds'.format(time.time()-last_time))
+#        last_time = time.time()
         new_screen = process_img(screen)
         cv2.imshow('window', new_screen)
         #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        if cv2.waitKey(delay=25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
         
